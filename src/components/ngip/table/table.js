31cf42f8542061c5fb1f1ca1ceec0ab675f9bb26
tirth0 <tirtharajsengupta@gmail.com>
2021-03-19 10:31:31 +0530

    minor changes

--- src/components/ngip/table/table.js
+++ src/components/ngip/table/table.js
@@ -8,8 +8,8 @@ export default function Table({n,dataX,dataY}) {
     const constructTable = ()=>{
         const tbl = []
         for (let i=0;i<=n;i++){
-            if (i==0) {tbl[i] = [0,0.25,0.50,0.75,1.00].reverse();continue;}
-            if (i==1) {tbl[i] = [1,1.5314,2.1281,2.7984,3.5597].reverse();continue}
+            if (i==0) {tbl[i] = dataX.reverse();continue;}
+            if (i==1) {tbl[i] = dataY.reverse();continue}
             else tbl[i] = []
             for (let j=0;j<n-i+1;j++){
                 tbl[i][j] = tbl[i-1][j] - tbl[i-1][j+1]
